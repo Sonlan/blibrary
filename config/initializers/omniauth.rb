5cb821e2644463a0543a207635cc189d790de349
Sonlan <1147649695@qq.com>
2018-08-20 11:18:17 +0800

    Update omniauth.rb
    fix: change the repuire bauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-require 'omniauth/strategies/gcac'
+require 'omniauth/strategies/bauth'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :bauth, client_id: Rails.configuration.setting[:auth][:app_id], client_secret: Rails.configuration.setting[:auth][:app_secret]
 end
